f0ab6526aeab6f367b300e35ca80cfb9f30b0936
Erick Rodrigues <149729630+erickki@users.noreply.github.com>
2025-11-15 17:17:22 -0300

    Update
    Instalação e configuração Tailwind CSS Animated.
    
    15/11/2025 - 17:17

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,17 @@
 export default function App() {
   return (
-    <div className="h-screen w-screnn flex flex-col justify-center items-center bg-tcinza-1">
+    <div className="h-screen w-screnn flex flex-col justify-center items-center gap-2 bg-tcinza-1">
       <h1 className="text-troxo-1/90 font-[Roboto] font-bold text-5xl">
         Testando o Vite
       </h1>
       <p className="text-troxo-1/70 font-[Roboto] font-bold text-2xl">
-        e o Tailwind CSS
+        Testando o Tailwind CSS
+      </p>
+      <p
+        className=" text-troxo-1/50 font-[Roboto] font-bold text-2xl animate-bounce animate-infinite
+        animate-ease-in-out"
+      >
+        Testando o Tailwind CSS Animated
       </p>
     </div>
   );
